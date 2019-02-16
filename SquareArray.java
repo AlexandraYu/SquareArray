dc6bee8608d59cadb2bb5d200189a1f5691afb25
AlexandraYu <beadsbaby@hotmail.com>
2019-02-16 17:03:30 +0800

    added a optimized solution function and corrected the input array to suit the description of the problem
    1. Due to the given array has to be none descending order, there's a way to optimize the solution, added sortSquareArray function
    2. Correct the input function; it has to be none descending like described in the problem.

--- SquareArray.java
+++ SquareArray.java
@@ -29,10 +29,30 @@ public class SquareArray{
         return A; 
     }
     public static void main(String []args){
-        int[] array = {-1, 5, -6, 9, 10, 12, 11, -10}; 
+        int[] array = {-10, -5, -1, 3, 5, 12, 14}; 
         sortedSquares(array);
         for (int i=0; i<array.length; i++) {
             System.out.println(array[i]+" ");
         }
     }
+    //There's actually another way to do it, with much better O(n) performance. The point is that the starting array is ***sorted*** in non descending order
+    //Remember, the smallest negative number will become biggest positive number when squared, since the array given is sorted in non descending order,
+    //that means you can compare "head" and "tail" elements in an array and decide which to put in the "solution array" first.  
+    public static int[] sortSquareArray(int[] A) {
+         int n = A.length; 
+         int[] solution = new int[n];
+         int i=0, j=n-1; 
+         int tmp = 0; 
+         for(int count=n-1; count>=0; count--){
+             if (Math.abs(A[i]) > Math.abs(A[j])) {
+                 solution[count] = A[i]*A[i];
+                 i++; 
+             }
+             else {
+                 solution[count] = A[j]*A[j]; 
+                 j--; 
+             }
+         }
+         return solution;
+     }
 }
